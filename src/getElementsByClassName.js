72a8e8c6e8326d80c4ce9a36a88779a3ec1f1ef2
Hack Reactor Student <student@hackreactor.com>
2017-08-14 12:23:48 -0500

    updated recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,25 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+ // create an empty array
+  var classNameArray = [];
+  
+  //Iterate through the DOM
+  
+  for (var i = 0; i < document.body.childNodes.length; i++) {
+  //  console.log(document.childNodes[i]);
+    if (document.childNodes[i] > 0) {
+      getElementsByClassName(document.childNodes[i]);
+    } else if (document.childNodes[i].classList) {
+      console.log(document.childNodes[i].classList);
+       
+    }   
+  }                            
+
+
+  return classNameArray; 
+ // return all of the elements matching the classname given
 };
+
+
+getElementsByClassName('cats');
